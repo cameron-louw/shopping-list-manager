340245edbb45a357a1f7b41f9d34b87de5280582
cameron-louw <cameronlouw89@icloud.com>
2023-05-08 18:48:21 +1200

    Add 3 responsive list boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="lists-container">
+      <div className="list">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <strong>LIST 1</strong>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="list">
+        <p>
+          <strong>LIST 2</strong>
+        </p>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+      </div>
+      <div className="list">
+        <p>
+          <strong>LIST 3</strong>
+        </p>
+      </div>
+    </div>
   )
 }
 
